e45a463e5ab4968d1f4fd2c7820c7ba038a3e780
Stephanie <stmiller1998@live.com>
2024-05-15 20:16:16 -0700

    fixed error in files

--- blogging/views.py
+++ blogging/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
+from django.views.generic import ListView, DetailView
 from blogging.models import Post
 
 class PostListView(ListView):
